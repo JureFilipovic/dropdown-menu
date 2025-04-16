ae17aa524836d58ed0d2353a510ba15e31bf70c2
JureFilipovic <jure.filipovic@gmail.com>
2025-04-16 14:50:53 +0200

    Make it able to build

--- src/dropdown-module.js
+++ src/dropdown-module.js
@@ -1,25 +1,25 @@
 export default function dropdownInit() {
-    const dropdowns = document.querySelectorAll(".dropdown");
-  
-    dropdowns.forEach((dropdown) => {
-      const button = dropdown.querySelector(".dropbtn");
-      const content = dropdown.querySelector(".dropdown-content");
-  
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  dropdowns.forEach((dropdown) => {
+    const button = dropdown.querySelector(".dropbtn");
+    const content = dropdown.querySelector(".dropdown-content");
+
+    content.classList.add("hidden");
+
+    button.addEventListener("click", (e) => {
+      document.querySelectorAll(".dropdown-content").forEach((c) => {
+        if (c !== content) c.classList.add("hidden");
+      });
+
+      content.classList.toggle("hidden");
+      e.stopPropagation();
+    });
+  });
+
+  document.addEventListener("click", () => {
+    document.querySelectorAll(".dropdown-content").forEach((content) => {
       content.classList.add("hidden");
-  
-      button.addEventListener("click", (e) => {
-        document.querySelectorAll(".dropdown-content").forEach((c) => {
-          if (c !== content) c.classList.add("hidden");
-        });
-  
-        content.classList.toggle("hidden");
-        e.stopPropagation();
-      });
     });
-  
-    document.addEventListener("click", () => {
-      document.querySelectorAll(".dropdown-content").forEach((content) => {
-        content.classList.add("hidden");
-      });
-    });
-  }
+  });
+}

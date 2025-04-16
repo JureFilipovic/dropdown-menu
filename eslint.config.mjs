84e8a6b7731c44f6305ba210192d00a0f881e166
JureFilipovic <jure.filipovic@gmail.com>
2025-04-16 11:55:16 +0200

    Make it work with prettier plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
 import js from "@eslint/js";
 import globals from "globals";
+import prettierPlugin from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { rules: { "prettier/prettier": "error" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 ]);

dc980060959e483b2b6c6a33fa6d498dc1a23074
JureFilipovic <jure.filipovic@gmail.com>
2025-04-16 14:16:40 +0200

    Make dropdown module
    Dropdown module is able to handle as many dropdown
    menus as needed. Menus are opened on click and closed
    if clicked anywhere on screen or on other dropdown
    menu.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,24 @@
 import js from "@eslint/js";
-import globals from "globals";
+import pkg from "globals";
+const { node, browser } = pkg;
 import prettierPlugin from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: { ...browser, ...node }, // Combining both globals
+      parserOptions: {
+        ecmaVersion: "latest", // You can specify ECMAScript version here
+        sourceType: "module", // This enables ES module syntax
+      },
+    },
     plugins: {
       js,
       prettier: prettierPlugin,
     },
     extends: ["js/recommended"],
-    languageOptions: {
-      globals: globals.browser,
-    },
     rules: {
       "prettier/prettier": "error",
     },
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import dropdownInit from "./dropdown-module";
+
+dropdownInit();
